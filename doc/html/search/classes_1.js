22dd459847eb697b4f0f3b05709a05b7c68001a5
Eren Sefer <esefer@student.tgm.ac.at>
2016-04-17 23:15:30 +0200

    doc created with doxygen

---
+++ doc/html/search/classes_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['connectionwidget',['ConnectionWidget',['../class_connection_widget.html',1,'']]],
+  ['custommodel',['CustomModel',['../class_custom_model.html',1,'']]]
+];
---
+++ doc/html/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "bcq",
+  1: "bcq"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes"
+};
+
